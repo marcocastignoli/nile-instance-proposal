ad4fd2467b42ac1a45b85e34a4f2ea9d13618ac7
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-10-29 12:33:27 +0100

    Consistent use of async/await instead of promises
    Saw you used async/await in another file, and I think consistency is important, so therefore I propose using async/await here as well. If you prefer promises though, discard this PR and update here https://github.com/marcocastignoli/nile-instance-proposal/blob/master/InstanceHttpProtocol.js#L5.

--- Instance.js
+++ Instance.js
@@ -7,19 +7,17 @@ class Instance {
     loadListeners(protocol) {
         this.api.onNodeRegister(protocol).then(this.onRegisterNode)
     }
-    onRegisterNode({ information, hash }) {
+    async onRegisterNode({ information, hash }) {
         let ws_id_confirm = ws_id
         // register node then send confirmation
-        this.api.toNodeRegistered(token)
-            // check if confirmation was received
-            .then(({ ws_id, confirm }) => {
-                if (ws_id == ws_id_confirm) {
-                    if (confirm) {
-                        console.log(`Node "${ws_id}" confirmed registration`)
-                    } else {
-                        console.log(`Node "${ws_id}" didn't confirm registration`)
-                    }
-                }
-            })
+        const { ws_id, confirm } = await this.api.toNodeRegistered(token)
+
+        if (ws_id == ws_id_confirm) {
+            if (confirm) {
+                console.log(`Node "${ws_id}" confirmed registration`)
+            } else {
+                console.log(`Node "${ws_id}" didn't confirm registration`)
+            }
+        }
     }
 }
